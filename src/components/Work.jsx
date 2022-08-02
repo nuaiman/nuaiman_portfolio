87b43718404e5b612b7284989b5ec5f41bcafb1f
Nuaiman Ashiq <ashiqnuaiman@gmail.com>
2022-08-02 01:07:28 -0700

    first commit

---
+++ src/components/Work.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import WorkoutBuddy from '../assets/workoutbuddy.PNG';
+import Tiktok from '../assets/tiktok.png';
+import Whatsapp from '../assets/whatsapp.png';
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+            Work
+          </p>
+          <p className='py-6'>// Check out some of my recent work</p>
+        </div>
+
+        {/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+          {/* 1st  Grid Item start */}
+          <div
+            style={{ backgroundImage: `url(${WorkoutBuddy})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                WorkoutBuddy - Woerkout logger
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/nuaiman/mern-WorkoutBuddy' target={'_blank'} rel="noreferrer">
+
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* 1st  Grid Item end */}
+
+
+
+          {/* 2st  Grid Item start */}
+          <div
+            style={{ backgroundImage: `url(${Tiktok})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Tiktok Clone (in pregress)
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/nuaiman' target={'_blank'} rel="noreferrer">
+
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* 2st  Grid Item end */}
+
+          {/* 3st  Grid Item start */}
+          <div
+            style={{ backgroundImage: `url(${Whatsapp})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Whatsapp Clone (in pregress)
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/nuaiman' target={'_blank'} rel="noreferrer">
+
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* 1st  Grid Item end */}
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
